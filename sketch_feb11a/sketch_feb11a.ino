698b95141e0c0b427806057fa954503ca3709867
raphaelkaramagi <karamagiraphael@gmail.com>
2026-02-11 15:03:10 -0500

    .

---
+++ sketch_feb11a/sketch_feb11a.ino
@@ -0,0 +1,50 @@
+//Pins for QTI connections on board
+#define lineSensor1 47 
+#define lineSensor2 49 
+#define lineSensor3 51 
+
+void setup() {
+  Serial.begin(9600); //start the serial monitor so we can view the output
+  int state = 0;
+}
+
+void loop() {
+  int qti1 = rcTime(lineSensor1); 
+  Serial.print("qti1 ");
+  Serial.println(light(qti1)); 
+
+  int qti2 = rcTime(lineSensor2); 
+  Serial.print("qti2 ");
+  Serial.println(light(qti2)); 
+
+  int qti3 = rcTime(lineSensor3); 
+  Serial.print("qti3 ");
+  Serial.println(light(qti3)); 
+
+  
+  
+  delay(200);
+}
+
+//Defines funtion 'rcTime' to read value from QTI sensor
+// From Ch. 6 Activity 2 of Robotics with the BOE Shield for Arduino
+long rcTime(int pin){
+  pinMode(pin, OUTPUT);    // Sets pin as OUTPUT
+  digitalWrite(pin, HIGH); // Pin HIGH
+  delay(1);                // Waits for 1 millisecond
+  pinMode(pin, INPUT);     // Sets pin as INPUT
+  digitalWrite(pin, LOW);  // Pin LOW
+  long time = micros();    // Tracks starting time
+  while(digitalRead(pin)); // Loops while voltage is high
+  time = micros() - time;  // Calculate decay time
+  return time;             // Return decay time
+}
+
+int light(long rcTime){
+  if(rcTime < 150){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
